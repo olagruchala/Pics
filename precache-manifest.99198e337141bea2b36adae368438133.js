c23bc714cb418067322d7f9511a1718ad4fe9cda
Ola <aleksandra@gruchala.eu>
2019-12-02 18:50:54 +0100

    Updates

---
+++ precache-manifest.99198e337141bea2b36adae368438133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfaab1d562be2d1eaab2c18d2b61ded",
+    "url": "/Pics/index.html"
+  },
+  {
+    "revision": "242f3d9d358108b6a341",
+    "url": "/Pics/static/css/main.00c2ba74.chunk.css"
+  },
+  {
+    "revision": "33687cd77d31663882cb",
+    "url": "/Pics/static/js/2.b8dd60fb.chunk.js"
+  },
+  {
+    "revision": "242f3d9d358108b6a341",
+    "url": "/Pics/static/js/main.398cd3fe.chunk.js"
+  },
+  {
+    "revision": "c2dc24c9cd4dc8c23c47",
+    "url": "/Pics/static/js/runtime-main.d0edcd03.js"
+  }
+]);
